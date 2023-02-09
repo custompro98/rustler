d3f6b710adb6ee15aed27ea1626819c2841f618d
custompro98 <mitchjoa@gmail.com>
2023-02-09 16:55:19 -0500

    Cooler pattern matching

--- src/roller.rs
+++ src/roller.rs
@@ -5,47 +5,47 @@ use crate::grpc::{dice, Dice};
 type Generator = fn(i32) -> i32;
 
 pub fn roll(dice: Dice, gen: Generator) -> Result<Dice, Box<dyn error::Error>> {
-    let mut interim = Vec::<i32>::new();
+    let mut rolls = Vec::<i32>::new();
 
     for _ in 0..dice.count {
-        let mut res = 0;
+        let mut result = 0;
 
         loop {
             let roll = gen(dice.faces);
-            res += roll;
+            result += roll;
 
             if !dice.explodes_ons.contains(&roll) {
                 break;
             }
         }
 
-        interim.push(res)
+        rolls.push(result)
     }
 
+    rolls = match dice.optional_drop_n {
+        Some(ref drop_n) => {
+            rolls.sort();
 
-    match dice.optional_drop_n {
-        Some(dice::OptionalDropN::DropLowest(n)) => {
-            if n <= interim.len() as i32 {
-                println!("Dropping lowest {} from {:?}", n, interim);
-                interim.sort();
-                interim = interim[n as usize..interim.len()].to_vec();
-            }
-        },
-        Some(dice::OptionalDropN::DropHighest(n)) => {
-            if n <= interim.len() as i32 {
-                println!("Dropping highest {} from {:?}", n, interim);
-                interim.sort();
-                interim = interim[0..interim.len() - n as usize].to_vec();
+            match drop_n {
+                dice::OptionalDropN::DropHighest(n) => {
+                    rolls.reverse();
+                    drop(*n, rolls)
+                }
+                dice::OptionalDropN::DropLowest(n) => drop(*n, rolls),
             }
         }
-        None => {},
-    }
+        None => rolls,
+    };
 
     Ok(Dice {
         faces: dice.faces().into(),
         count: dice.count,
-        result: interim.iter().sum(),
+        result: rolls.iter().sum(),
         optional_drop_n: dice.optional_drop_n,
         explodes_ons: dice.explodes_ons,
     })
+}
+
+fn drop(n: i32, v: Vec<i32>) -> Vec<i32> {
+    v[n as usize..v.len()].to_vec()
 }
